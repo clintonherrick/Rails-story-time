27e304e0562bcf5756be96a3662a127363c46acf
Clinton Herrick & Vaughn Dice <student@epicodus.com>
2015-10-14 10:52:46 -0700

    add REST actions/routes for story

---
+++ app/controllers/stories_controller.rb
@@ -0,0 +1,48 @@
+class StoriesController <ApplicationController
+
+  def index
+    @stories = Story.all
+  end
+
+  def show
+    @story = Story.find(params[:id])
+  end
+
+  def new
+    @story = Story.new
+  end
+
+  def create
+    @story = Story.new(story_params)
+    if @story.save
+      redirect_to story_path(@story)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @story = Story.find(params[:id])
+    @story.destroy
+
+    redirect_to stories_path
+  end
+
+  def edit
+    @story = Story.find(params[:id])
+  end
+
+  def update
+    @story = Story.find(params[:id])
+    if @story.update(story_params)
+      redirect_to story_path(@story)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def story_params
+    params.require(:story).permit(:title)
+  end
+end
